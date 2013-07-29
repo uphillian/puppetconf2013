c07e85a910361a8a07f5e70481ae8892e2beca99
Thomas Uphill <thomas@narrabilis.com>
2013-07-28 23:29:29 -0700

    Adding cidr definition

--- puppet/modules/dns/lib/facter/reverse_eth0.rb
+++ puppet/modules/dns/lib/facter/reverse_eth0.rb
@@ -1,11 +1,18 @@
 # reverse.rb
 # Set a fact for the reverse lookup of the network on eth0
 
-require('ipaddr')
+require 'ipaddr'
+require 'puppet/util/ipcidr'
+
 
 ip = IPAddr.new(Facter.value('network_eth0'))
+nm = Puppet::Util::IPCidr.new(Facter.value('network_eth0')).mask(Facter.value('netmask_eth0'))
+cidr = nm.cidr
 
 # return the reverse to facter
 Facter.add("reverse_eth0") do
   setcode do ip.reverse.to_s[2..-1] end
+end
+Facter.add("netmask_cidr_eth0") do
+  setcode do cidr end
 end
